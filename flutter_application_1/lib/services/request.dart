343038ad55c4e8a54405624127eb95eae9ab1c5b
Moustafa El-Shaboury <moustafa.el-shaboury@dvla.gov.uk>
2026-06-28 18:54:04 +0100

    Added error handling to request.dart

--- flutter_application_1/lib/services/request.dart
+++ flutter_application_1/lib/services/request.dart
@@ -10,14 +10,19 @@ class JsonRequest {
   Future<void> getData() async {
     // Since we're awaiting this async function elsewhere, we must have it be of type Future<void>. Can only await functions with a future. A future in dart is a promise in JS
     // See this error: Uses 'await' on an instance of 'void', which is not a subtype of 'Future'. https://dart.dev/diagnostics/await_only_futures
+    try {
+      Uri url = Uri.parse('https://jsonplaceholder.typicode.com/todos/${number.toString()}');
+      Response response = await get(url);
+      // Response type given by http. Contains info about response. Body is what we want.
+      print(response.body);
+      // This body will look a map but it's actually just a string. 
+      // Can't call fields in there directly, so response.body.userId won't work. Must first convert:
+      Map body = jsonDecode(response.body);
+      data = 'UserID: ${body['title']}';
 
-    Uri url = Uri.parse('https://jsonplaceholder.typicode.com/todos/${number.toString()}');
-    Response response = await get(url);
-    // Response type given by http. Contains info about response. Body is what we want.
-    print(response.body);
-    // This body will look a map but it's actually just a string. 
-    // Can't call fields in there directly, so response.body.userId won't work. Must first convert:
-    Map body = jsonDecode(response.body);
-    data = 'UserID: ${body['title']}';
+    } catch (e) {
+      data = 'caught error $e';
+    }
+
   }
 }
